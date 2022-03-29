c3df387c7919a32aee8b1ad68f111b9ab319dd38
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-03-29 15:18:20 -0500

    Main elements added

---
+++ src/templates/Header.js
@@ -0,0 +1,18 @@
+const Header = () => {
+    const view = /*html*/`
+    <div class="Header-main">
+        <div class="Header-logo">
+            <h1>
+                <a href="/">PICKLE RICKKKK</a>
+            </h1>
+        </div>
+    </div>
+    <div class="Header-nav">
+        <a href="#/about">About</a>
+    </div>
+    `;
+
+    return view;
+};
+
+export default Header;
