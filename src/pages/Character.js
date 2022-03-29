49b95318eb28cd88bc97c5f87f01cfce1aa62720
Santiago Torres <santiago.torres1234.stg@gmail.com>
2022-03-29 18:25:49 -0500

    Build for deploy

--- src/pages/Character.js
+++ src/pages/Character.js
@@ -6,13 +6,13 @@ const Character = async () => {
     const id = getHash();
     const character = await getData(id);
     const view = /*html*/`
-        <div class="Character-inner">
+        <div class="Characters-inner">
             <article class="Characters-card">
                 <img src="${character.image}" alt="${character.name}">
                 <h2>${character.name}</h2>
             </article>
 
-            <article class="Character-card">
+            <article class="Characters-card">
                 <h3>Episodes: <span>${character.episode.length}</span></h3>
                 <h3>Status: <span>${character.status}</span></h3>
                 <h3>Species: <span>${character.species}</span></h3>
